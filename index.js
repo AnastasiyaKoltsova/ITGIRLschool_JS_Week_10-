37f22b1daf397f716cd3bfb2e7a4ff9821d1a88a
Anastasiya Koltsova <Koltsova_av@mail.ru>
2023-02-17 12:17:40 +0300

    create gallery

---
+++ index.js
@@ -0,0 +1,8 @@
+let img1 = document.getElementById('img1');
+function getPicture() {
+    img1.src = './images/img1.jpg';
+};
+
+function getSecondPicture() {
+    img1.src = './images/img2.jpg';
+};
